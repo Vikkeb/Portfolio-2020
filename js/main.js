19480c498ba5eec55caf16f265ff88ffc548b403
Victor Bergsten <victorbergsten92@gmail.com>
2020-03-27 21:57:37 +0100

    Added back to top button

--- js/main.js
+++ js/main.js
@@ -7,3 +7,18 @@ $('.nallebudet_btn').modaal();
 
 
 });
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
