d87a2bdd6d826f9de9ec3522760b57391157e6aa
Vikkeb <victorbergsten@spray.se>
2023-03-24 15:17:03 +0100

    Responsive changes to font and bg

--- js/main.js
+++ js/main.js
@@ -1,34 +1,19 @@
 jQuery(document).ready(function($) {
+  // Initialize modaal for all buttons with a single line
+  $('.scania_btn, .ericsson_btn, .lifesymb_btn, .leovegas_btn, .nallebudet_btn, .solarsupply_btn, .golf_btn').modaal();
 
-$('.scania_btn').modaal();
-$('.ericsson_btn').modaal();
-$('.lifesymb_btn').modaal();
-$('.leovegas_btn').modaal();
-$('.nallebudet_btn').modaal();
-$('.solarsupply_btn').modaal();
-$('.golf_btn').modaal();
+  const btn = $('#button');
 
+  $(window).scroll(function() {
+    // Toggle 'show' class based on the scroll position
+    btn.toggleClass('show', $(window).scrollTop() > 300);
+  });
 
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop: 0}, '300');
+  });
 
-});
-
-var btn = $('#button');
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
-
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-	//The first argument are the elements to which the plugin shall be initialized
-	//The second argument has to be at least a empty object or a object with your desired options
-	OverlayScrollbars(document.querySelectorAll("body"), { });
+  // Initialize OverlayScrollbars for the body element
+  OverlayScrollbars(document.querySelector("body"), {});
 });
