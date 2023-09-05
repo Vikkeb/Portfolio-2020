c4bc8f15bb84fff434c48e49e6bc5b0c08de9c4d
Vikkeb <victorbergsten@spray.se>
2023-09-05 15:51:18 +0200

    Added modals and content to Go Connect & Seropian

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
   // Initialize modaal for all buttons with a single line
-  $('.scania_btn, .ericsson_btn, .lifesymb_btn, .leovegas_btn, .nallebudet_btn, .solarsupply_btn, .golf_btn').modaal();
+  $('.seropian_btn, .goconnect_btn, .scania_btn, .ericsson_btn, .lifesymb_btn, .leovegas_btn, .nallebudet_btn, .solarsupply_btn, .golf_btn').modaal();
 
   const btn = $('#button');
 
