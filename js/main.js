b420a73b140ad6df2685af634fd2f17385101542
victor bergsten <victorbergsten92@gmail.com>
2024-01-03 14:29:53 +0100

    Added modaal replacement with dialog

--- js/main.js
+++ js/main.js
@@ -17,4 +17,10 @@ jQuery(document).ready(function($) {
   // simple initialization with an element
   const osInstance = OverlayScrollbars(document.querySelector('body'), {});
 
+    // Get the dialog container HTML element (with the accessor method you want)
+  const element = document.getElementById('goconnect')
+
+  // Instantiate a new A11yDialog module
+  const dialog = new A11yDialog(element)
+
 });
