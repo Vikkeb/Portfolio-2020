56888f6557a6e829948a42ce4d0b87233efdd839
victor bergsten <victorbergsten92@gmail.com>
2024-01-03 16:53:15 +0100

    wokring on body element now

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   const element = document.getElementById('goconnect');
 
   // Initialize OverlayScrollbars
-  const osInstance = new OverlayScrollbars(document.querySelector('#content'), {});
+  const osInstance = new OverlayScrollbars(document.querySelector('body'), {});
   
   // Initialize A11yDialog
   const dialog = new A11yDialog(element);
