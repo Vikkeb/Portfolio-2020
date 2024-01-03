ba1878e6b71044de4fb00920adfd93b92911e159
victor bergsten <victorbergsten92@gmail.com>
2024-01-03 15:07:12 +0100

    changed order of js

--- js/main.js
+++ js/main.js
@@ -3,11 +3,8 @@ jQuery(document).ready(function($) {
   // Get the dialog container HTML element (with the accessor method you want)
   const element = document.getElementById('goconnect');
 
-    // Get the body element
-    const body = document.body;
-  
-    // Initialize OverlayScrollbars on the body element
-    const osInstance = new OverlayScrollbars(body, {});  
+  // Initialize OverlayScrollbars on the element after the document is ready
+  const osInstance = new OverlayScrollbars('body');
 
   // Instantiate a new A11yDialog module
   const dialog = new A11yDialog(element);
