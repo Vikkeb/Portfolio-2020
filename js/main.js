7c3896fb2ee3a34be96939ab0138f460a2647f79
victor bergsten <victorbergsten92@gmail.com>
2024-01-03 14:54:14 +0100

    removed overlay scrollbars for testing

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 jQuery(document).ready(function($) {
 
-  // simple initialization with an element
-  const osInstance = OverlayScrollbars(document.querySelector('body'), {});
-
   // Get the dialog container HTML element (with the accessor method you want)
   const element = document.getElementById('goconnect')
 
