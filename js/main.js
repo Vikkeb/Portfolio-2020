922c36e0760c774d27f1b523f0322c66b8344713
victor bergsten <victorbergsten92@gmail.com>
2024-01-03 17:09:16 +0100

    fixed scrollbar

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ $(document).ready(function() {
   const element = document.getElementById('goconnect');
 
   // Initialize OverlayScrollbars
-  const osInstance = new OverlayScrollbars(document.querySelector('body'), {});
+  const osInstance = new OverlayScrollbars(document.querySelector('body'), {
+    
+    className : "os-theme-dark"
+  
+  });
   
   // Initialize A11yDialog
   const dialog = new A11yDialog(element);
