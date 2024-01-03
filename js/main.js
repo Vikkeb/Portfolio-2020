0ef88a9a88b99601c6b81c832a1bd72addc8098a
victor bergsten <victorbergsten92@gmail.com>
2024-01-03 15:05:44 +0100

    get body element in js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@ jQuery(document).ready(function($) {
   // Get the dialog container HTML element (with the accessor method you want)
   const element = document.getElementById('goconnect');
 
-  // Initialize OverlayScrollbars on the element after the document is ready
-  const osInstance = new OverlayScrollbars('body');
+    // Get the body element
+    const body = document.body;
+  
+    // Initialize OverlayScrollbars on the body element
+    const osInstance = new OverlayScrollbars(body, {});  
 
   // Instantiate a new A11yDialog module
   const dialog = new A11yDialog(element);
