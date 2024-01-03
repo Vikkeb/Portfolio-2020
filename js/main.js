78adde044b5b27bad0d9143b43ab5f2e8f0afbd8
victor bergsten <victorbergsten92@gmail.com>
2024-01-03 16:35:33 +0100

    fixed overlayscrollbars somewhat

--- js/main.js
+++ js/main.js
@@ -1,25 +1,9 @@
 $(document).ready(function() {
 
-  console.log(OverlayScrollbars); // Log OverlayScrollbars
-  console.log(A11yDialog); // Log A11yDialog
+    const element = document.getElementById('goconnect');
 
-
-  const element = document.getElementById('goconnect');
-
-  console.log('Checking OverlayScrollbars script...');
-
-  if (typeof OverlayScrollbars === 'undefined') {
-    console.log('OverlayScrollbars is not loaded yet');
-    document.querySelector('script[src*="overlayscrollbars"]').addEventListener('load', function() {
-      console.log('OverlayScrollbars script has loaded');
-      const osInstance = new OverlayScrollbars(document.body);
-      const dialog = new A11yDialog(element);
-    });
-  } else {
-    console.log('OverlayScrollbars is already loaded');
-    const osInstance = new OverlayScrollbars(document.body);
+    const osInstance = OverlayScrollbars(document.querySelector('body'), {});
+    
     const dialog = new A11yDialog(element);
-  }
+    
 });
-
-
