aa887b06b3057b87e7ecf2a7aaa081b0a92bd50e
victor bergsten <victorbergsten92@gmail.com>
2024-01-03 14:36:37 +0100

    Moved js import to head

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
   // simple initialization with an element
   const osInstance = OverlayScrollbars(document.querySelector('body'), {});
 
-    // Get the dialog container HTML element (with the accessor method you want)
+  // Get the dialog container HTML element (with the accessor method you want)
   const element = document.getElementById('goconnect')
 
   // Instantiate a new A11yDialog module
